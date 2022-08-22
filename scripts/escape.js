04a8229f10480f98723d9d283073117518e80395
HsiaoHung Wang <wang@ispan.com.tw>
2022-08-22 14:00:23 +0800

    20220822AM

---
+++ scripts/escape.js
@@ -0,0 +1,10 @@
+console.log("C:\\shared\\JavaScript");
+//console.log("C:\shared\Java"Script");  //Uncaught SyntaxError: missing ) after argument list
+console.log('C:\shared\Java"Script');
+console.log("Mary's store");
+console.log("C:\shared\Java\"Script");
+console.log('Mary\'s store');
+console.log("aaaa\nbbbb");   //enter 
+console.log("aaaa\tbbbb");   //tab
+console.log("aaaa\vbbbb");   //v tab
+console.log("abc\bdefg") //backspace
