bbab6895b71bf9f5a45547ccf5fb3a5d24a6dcd8
HsiaoHung Wang <wang@ispan.com.tw>
2022-08-29 10:58:03 +0800

    Email格式檢查

--- scripts/validation.js
+++ scripts/validation.js
@@ -6,3 +6,15 @@ function checkEmpty(item) {
     }
     return isValid;
 }
+//mail="abcd"  4 0-3
+function checkMail(mail) {
+    let isValid = true;
+    if (!checkEmpty(mail)) {
+        isValid = false;
+    } else if (mail.indexOf("@") === -1 || mail.indexOf("@") === 0 || mail.indexOf("@") !== mail.lastIndexOf("@") || mail.indexOf("@") === mail.length - 1) {
+        isValid = false;
+    } else if (mail.indexOf(".") === -1 || mail.indexOf(".") === 0 || mail.indexOf(".") === mail.length - 1) {
+        isValid = false;
+    }
+    return isValid;
+}
