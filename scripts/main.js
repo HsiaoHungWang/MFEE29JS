a90771436b5dcb8a3d95da7d457e24856e154a08
HsiaoHung Wang <wang@ispan.com.tw>
2022-08-29 11:47:37 +0800

    計算年紀

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,12 @@ function temperatureConvert(degree, type) {
     }
     return degreeChange;
 }
+
+//計算年紀
+function calcAge(birthday) {
+    let birth = new Date(birthday);
+    let today = new Date();
+    //365.2425 * 24 * 60 * 60 * 1000
+    let age = Math.floor((today - birth) / (365.2425 * 24 * 60 * 60 * 1000));
+    return age;
+}
